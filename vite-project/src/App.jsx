f512f1dc52f87890e13ec16bc34dab68ae66fdb2
uy <a.normurzaev20@gmail.com>
2026-04-27 22:55:24 +0500

    raect boshlandi

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css"
+function App(){
+    return (
+        <>
+             <div className="header">
+              <h1>Hello world</h1>
+             </div>
+
+         </>
+    )
+}
+export default App
