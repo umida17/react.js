b3c9a52d2924106274b0aacf6c687f8f301c3e12
uy <a.normurzaev20@gmail.com>
2026-04-27 22:59:50 +0500

    header qismi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css"
 function App(){
     return (
         <> 
-        
+        <header>
+            <div className="container">
+                <div className="header-container">
+                    <img logo alt="" />
+                </div>
+            </div>
+        </header>
 
          </>
     )
