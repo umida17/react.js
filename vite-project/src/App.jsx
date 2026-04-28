e8fd66c56e6cf520505eaa643bead44cbb542607
uy <a.normurzaev20@gmail.com>
2026-04-28 20:53:12 +0500

    header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,6 +8,15 @@ function App(){
             <div className="container">
                 <div className="header-container">
                   <img src={logo} alt="" />
+                  <div className="registir">
+                    <a href="#">Home</a>
+                    <a href="#">Destinations</a>
+                    <a href="#">About</a>
+                    <a href="#">Partner</a>
+                     
+                  </div>
+                  <button>Login</button>
+                  <button>Register</button>
                 </div>
             </div>
         </header>
