c7ae0725dbe3fab1b8a79d72962e681242b521d9
uy <a.normurzaev20@gmail.com>
2026-04-28 22:35:50 +0500

    section2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -81,7 +81,10 @@ function App(){
                     <div className="section2-container">
                           <img src={travel2} alt="" />    
                           <div className="world">
-                            
+                            <h3>A new way to explore the world </h3>
+                            <p>For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect 
+                            trip, but now, they can also let Lonely Planet Experiences lead the way</p>
+                            <button>Learn more</button>
                             </div>  
                     </div>
                 </div>
